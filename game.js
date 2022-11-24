449980c5a56294d338e38512647d29363756523e
scotchmint <nullptr91@tuta.io>
2022-11-24 14:22:38 +0200

    Add get/show computer/player choise

--- game.js
+++ game.js
@@ -4,3 +4,37 @@ function getComputerChoise()
 
     return randomNumber;
 }
+
+function getPlayerChoise()
+{
+    return prompt("Choose: Rock / Paper / Scissors ?");
+}
+
+function showComputerChoise(computerSelection)
+{
+    switch (computerSelection)
+    {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
+function showPlayerChoise(playerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    switch (playerSelection)
+    {
+        case "rock":
+            return 0;
+        case "paper":
+            return 1;
+        case "scissors":
+            return 2;
+        default:
+            alert("Wrong input");
+    }
+}
