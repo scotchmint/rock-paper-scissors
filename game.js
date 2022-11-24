82c1a3d3c6121ec18a9cd40bc82a170c4668e3d7
scotchmint <nullptr91@tuta.io>
2022-11-24 17:41:21 +0200

    Add playRound and edit playerChoise

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ function getPlayerChoise()
 {
     let input = prompt("Choose: Rock / Paper / Scissors ?");
 
-    if((input === null) || (showChoise(input) === "Wrong input!"))
+    if((input === null) || (showChoise(input) === "wrong input"))
     {
         alert("Canceled");
     }
@@ -36,6 +36,36 @@ function showChoise(choise)
         case 2:
             return "Scissors";
         default:
-            return "Wrong input!";
+            return "wrong input";
+    }
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    let player = showChoise(playerSelection);
+    let computer = showChoise(computerSelection);
+
+    console.log(`You choose ${player}`);
+    console.log(`Computer choose ${computer}`);
+
+    if ((player === "Rock" && computer === "Rock")
+     || (player === "Paper" && computer === "Paper")
+     || (player === "Scissors" && computer === "Scissors"))
+    {
+        return "Tie";
+    }
+    else if ((player === "Rock" && computer === "Paper")
+          || (player === "Paper" && computer === "Scissors")
+          || (player === "Scissors" && computer === "Rock"))
+    {
+        return "Lose";
+    }
+    else if (player === "wrong input")
+    {
+        return "Try again!";
+    }
+    else
+    {
+        return "win";
     }
 }
