75f80e9fced4ff5b2a389d64c755ac84b0c63f66
scotchmint <nullptr91@tuta.io>
2022-12-05 20:58:18 +0200

    Change format

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rock Paper Scissors
 
-- This is a console version of the popular game Rock Paper Scissors made in: 24/11/2022
+<!-- - This is a console version of the popular game Rock Paper Scissors made in: 24/11/2022 -->
+- UI version made in: 5/12/2022
--- game.js
+++ game.js
@@ -21,7 +21,7 @@ function showChoiseInString(choise)
 const rock = document.querySelector('.btn-rock');
 const paper =document.querySelector('.btn-paper');
 const scissors = document.querySelector('.btn-scissors');
-const reset = document.querySelector('.btn-reset');
+const playAgain = document.querySelector('.btn-again');
 
 const compare = document.querySelector('.compare-choise');
 const playerResult = document.querySelector('.player-result');
@@ -38,97 +38,78 @@ computerResult.textContent = `Computer Score = ${computerScore}`;
 function playRound()
 {
     let computerChoise = showChoiseInString(getComputerChoise());
-    console.log(`comp choise: ${computerChoise}`);
 
     // player win scenario
-    // if (this === rock && computerChoise === 'scissors' && playerScore < 5 && computerScore < 5)
-    // {
-    //     ++playerScore;
-    //     compare.textContent = 'Rock beats Scissors';
-    //     roundResult.textContent = 'Player win this round';
-    // }
-    // else if (this === paper && computerChoise === 'rock' && playerScore < 5 && computerScore < 5)
-    // {
-    //     ++playerScore;
-    //     compare.textContent = 'Paper beats Rock';
-    //     roundResult.textContent = 'Player win this round';
-    // }
-    // else if (this === scissors && computerChoise === 'paper' && playerScore < 5 && computerScore < 5)
-    // {
-    //     ++playerScore;
-    //     compare.textContent = 'Scissors beats Paper';
-    //     roundResult.textContent = 'Player win this round';
-    // }
-
     if (playerScore < 5 && computerScore < 5)
     {
+
         if (this === rock && computerChoise === 'scissors')
-    {
+        {
         ++playerScore;
         compare.textContent = 'Rock beats Scissors';
         roundResult.textContent = 'Player win this round';
-    }
-    else if (this === paper && computerChoise === 'rock')
-    {
+        }
+        else if (this === paper && computerChoise === 'rock')
+        {
         ++playerScore;
         compare.textContent = 'Paper beats Rock';
         roundResult.textContent = 'Player win this round';
-    }
-    else if (this === scissors && computerChoise === 'paper')
-    {
+        }
+        else if (this === scissors && computerChoise === 'paper')
+        {
         ++playerScore;
         compare.textContent = 'Scissors beats Paper';
         roundResult.textContent = 'Player win this round';
-    }
-    }
+        }
 
-    // computer win scenario
-    if (this === scissors && computerChoise === 'rock' && playerScore < 5 && computerScore < 5)
-    {
+        // computer win scenario
+        if (this === scissors && computerChoise === 'rock')
+        {
         ++computerScore;
         compare.textContent = 'Rock beats Scissors';
         roundResult.textContent = 'Computer win this round';
         
-    }
-    else if (this === paper && computerChoise === 'scissors' && playerScore < 5 && computerScore < 5)
-    {
+        }
+        else if (this === paper && computerChoise === 'scissors')
+        {
         ++computerScore;
         compare.textContent = 'Scissors beats Paper';
         roundResult.textContent = 'Computer win this round';
-    }
-    else if (this === rock && computerChoise === 'paper' && playerScore < 5 && computerScore < 5)
-    {
+        }
+        else if (this === rock && computerChoise === 'paper')
+        {
         ++computerScore;
         compare.textContent = 'Paper beats Rock';
         roundResult.textContent = 'Computer win this round';
-    }
+        }
 
-    // tie scenario
-    if ((this === rock && computerChoise === 'rock') ||
-        (this === paper && computerChoise === 'paper') ||
-        (this === scissors && computerChoise === 'scissors') &&
-        (playerScore < 5 && computerScore < 5))
-    {
-        compare.textContent = 'Tie';
-    }
-
-    if (playerScore === 5)
-    {
-        finalResult.textContent = 'Player win!';
-        reset.classList.remove('hide');
-    }
-    else if (computerScore === 5)
-    {
-        finalResult.textContent = 'Computer win!';
-        reset.classList.remove('hide');
+        // tie scenario
+        if ((this === rock && computerChoise === 'rock') ||
+            (this === paper && computerChoise === 'paper') ||
+            (this === scissors && computerChoise === 'scissors') &&
+            (playerScore < 5 && computerScore < 5))
+        {
+            compare.textContent = 'Tie';
+        }
     }
 
     playerResult.textContent = `Player Score = ${playerScore}`;
     computerResult.textContent = `Computer Score = ${computerScore}`;
+
+    if (playerScore === 5)
+    {
+        finalResult.textContent = 'Player win!';
+        playAgain.classList.remove('hide');
+    }
+    else if (computerScore === 5)
+    {
+        finalResult.textContent = 'Computer win!';
+        playAgain.classList.remove('hide');
+    }
 }
 
 rock.addEventListener('click', playRound);
 paper.addEventListener('click', playRound);
 scissors.addEventListener('click', playRound);
 
-reset.addEventListener('click', () => location.reload());
+playAgain.addEventListener('click', () => location.reload());
