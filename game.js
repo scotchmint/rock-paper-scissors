876992c15c03cf013256d5de6860ffc345e0eb5d
scotchmint <nullptr91@tuta.io>
2022-12-07 13:32:48 +0200

    Add UI

--- game.js
+++ game.js
@@ -25,42 +25,66 @@ const playAgain = document.querySelector('.btn-again');
 
 const compare = document.querySelector('.compare-choise');
 const playerResult = document.querySelector('.player-result');
+const result = document.querySelector('.result');
 const computerResult = document.querySelector('.computer-result');
 const roundResult = document.querySelector('.round-result');
-const finalResult = document.querySelector('.final-result');
+
+const win = document.querySelector('.win');
+const lose = document.querySelector('.lose');
+
+const playerNumber = document.querySelector('.player-number');
+const computerNumber = document.querySelector('.computer-number');
+
+const playerStyle = document.querySelector('.player');
+const computerStyle = document.querySelector('.computer');
+
+const roundCounter = document.querySelector('.round-counter');
+const roundNumber = document.querySelector('.round-number');
 
 function game()
 {
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 
-playerResult.textContent = `Player Score = ${playerScore}`;
-computerResult.textContent = `Computer Score = ${computerScore}`;
+playerNumber.textContent = `${playerScore}`;
+computerNumber.textContent = `${computerScore}`;
+roundNumber.textContent = `${round}`;
 
 function playRound()
 {
+    result.removeAttribute('id');
     let computerChoise = showChoiseInString(getComputerChoise());
-
+    
     if (playerScore < 5 && computerScore < 5)
     {
+        ++round;
+        roundNumber.textContent = `${round}`;
+
         // player win scenario
         if (this === rock && computerChoise === 'scissors')
         {
         ++playerScore;
         compare.textContent = 'Rock beats Scissors';
-        roundResult.textContent = 'Player win this round';
+        compare.style.color = 'blue';
+        roundResult.textContent = 'You win this round';
+        roundResult.style.color = 'green';
         }
         else if (this === paper && computerChoise === 'rock')
         {
         ++playerScore;
         compare.textContent = 'Paper beats Rock';
-        roundResult.textContent = 'Player win this round';
+        compare.style.color = 'blue';
+        roundResult.textContent = 'You win this round';
+        roundResult.style.color = 'green';
         }
         else if (this === scissors && computerChoise === 'paper')
         {
         ++playerScore;
         compare.textContent = 'Scissors beats Paper';
-        roundResult.textContent = 'Player win this round';
+        compare.style.color = 'blue';
+        roundResult.textContent = 'You win this round';
+        roundResult.style.color = 'green';
         }
 
         // computer win scenario
@@ -68,20 +92,26 @@ function playRound()
         {
         ++computerScore;
         compare.textContent = 'Rock beats Scissors';
+        compare.style.color = 'crimson';
         roundResult.textContent = 'Computer win this round';
+        roundResult.style.color = 'red';
         
         }
         else if (this === paper && computerChoise === 'scissors')
         {
         ++computerScore;
         compare.textContent = 'Scissors beats Paper';
+        compare.style.color = 'crimson';
         roundResult.textContent = 'Computer win this round';
+        roundResult.style.color = 'red';
         }
         else if (this === rock && computerChoise === 'paper')
         {
         ++computerScore;
         compare.textContent = 'Paper beats Rock';
+        compare.style.color = 'crimson';
         roundResult.textContent = 'Computer win this round';
+        roundResult.style.color = 'red';
         }
 
         // tie scenario
@@ -91,21 +121,40 @@ function playRound()
             (playerScore < 5 && computerScore < 5))
         {
             compare.textContent = 'Tie';
+            compare.style.color = 'gray';
+            roundResult.textContent = '';
         }
     }
 
-    playerResult.textContent = `Player Score = ${playerScore}`;
-    computerResult.textContent = `Computer Score = ${computerScore}`;
+    playerNumber.textContent = `${playerScore}`;
+    computerNumber.textContent = `${computerScore}`;
+
+    if (roundResult.textContent.includes('You'))
+    {
+        playerStyle.style.backgroundColor = 'green';
+        computerStyle.style.backgroundColor = 'red';
+    }
+    else if (roundResult.textContent.includes('Computer'))
+    {
+        playerStyle.style.backgroundColor = 'red';
+        computerStyle.style.backgroundColor = 'green';
+    }
+    else if (compare.textContent.includes('Tie'))
+    {
+        playerStyle.style.backgroundColor = 'gray';
+        computerStyle.style.backgroundColor = 'gray';
+    }
 
     if (playerScore === 5)
     {
-        finalResult.textContent = 'Player win!';
+        win.removeAttribute('id');
         playAgain.classList.remove('hide');
+        playAgain.removeAttribute('id');
     }
     else if (computerScore === 5)
     {
-        finalResult.textContent = 'Computer win!';
-        playAgain.classList.remove('hide');
+        lose.removeAttribute('id');
+        playAgain.removeAttribute('id');
     }
 }
 
